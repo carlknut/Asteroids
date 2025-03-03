bb1999804bfc579c323eec666b3769cad6ee55a5
carlknut <carl.knutson17@gmail.com>
2025-03-03 09:52:19 -0600

    added constants.py and .gitignore

--- main.py
+++ main.py
@@ -2,9 +2,25 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    
     print("Starting Asteroids!")
+    print("Screen width: 1280: " + str(SCREEN_WIDTH) )
+    print("Screen height: 720" + str(SCREEN_HEIGHT) )
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(Black)
+
+        display.flip()
 
 if __name__ == "__main__":
     main()
